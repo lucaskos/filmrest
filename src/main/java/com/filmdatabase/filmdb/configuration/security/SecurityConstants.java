d5abf263fa9dfb6ba2fe8db85ef699dfdef24c00
lkosmala <kosmala.luke@gmail.com>
2018-02-10 15:39:20 +0100

    Added constants to avoid hardcode

--- src/main/java/com/filmdatabase/filmdb/configuration/security/SecurityConstants.java
+++ src/main/java/com/filmdatabase/filmdb/configuration/security/SecurityConstants.java
@@ -1,9 +1,51 @@
 package com.filmdatabase.filmdb.configuration.security;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SecurityConstants {
     public static final String SECRET = "SecretKeyToGenJWTs";
     public static final long EXPIRATION_TIME = 864_000_000; // 10 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users/sign-up";
+
+
+    public static final String ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
+    public static final String ALLOW_CREDENTIALS_VALUE = "true";
+    public static final String MAX_AGE = "Access-Control-Max-Age";
+    public static final String MAX_AGE_TIME = "3600";
+    public static final String METHODS_ALLOWED = "Access-Control-Allow-Methods";
+    public static final String HEADERS_ALLOWED = "Access-Control-Allow-Headers";
+
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String ORIGIN = "Origin";
+
+    private static List<String> headersList;
+
+    public static String getGeneratedHeaders() {
+
+        if(headersList == null || headersList.isEmpty()) {
+            headersList = generateHeadersList(AUTHORIZATION, ORIGIN);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String str : headersList) {
+            if (headersList.get(headersList.size()-1) != str) {
+                sb.append(str + ", ");
+            } else {
+                sb.append(str);
+            }
+        }
+        return sb.toString();
+    }
+
+    private static List<String> generateHeadersList(String... string) {
+        if (string != null) {
+            return Arrays.stream(string).collect(Collectors.toList());
+        }
+        return null;
+    }
 }
--- src/main/java/com/filmdatabase/filmdb/configuration/security/auth/JwtAuthenticationFilter.java
+++ src/main/java/com/filmdatabase/filmdb/configuration/security/auth/JwtAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.filmdatabase.filmdb.configuration.security.auth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.filmdatabase.filmdb.configuration.security.SecurityConstants;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -60,24 +61,29 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .signWith(SignatureAlgorithm.HS512, SECRET.getBytes())
                 .compact();
+
+        //todo wywalic 4 linijki nizej
+        //todo constantsy zamiast hardcodu
         String origin = req.getHeader("Origin");
         res.setHeader("Access-Control-Allow-Headers", HEADER_STRING + ", " + "Content-Type, Accept, X-Requested-With, remember-me");
         res.setHeader("Access-Control-Allow-Origin", allowedOrigins.contains(origin) ? origin : "");
         res.setHeader("Vary", "Origin");
 
         // Access-Control-Max-Age
-        res.setHeader("Access-Control-Max-Age", "3600");
+        res.setHeader(MAX_AGE, MAX_AGE_TIME);
 
         // Access-Control-Allow-Credentials
-        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader(ALLOW_CREDENTIALS, ALLOW_CREDENTIALS_VALUE);
 
         // Access-Control-Allow-Methods
-        res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        res.setHeader(METHODS_ALLOWED, "POST, GET, OPTIONS, DELETE");
+
+        String a = SecurityConstants.getGeneratedHeaders();
 
         // Access-Control-Allow-Headers
-        res.setHeader("Access-Control-Allow-Headers",
-                "Origin, Authorization, X-Requested-By, X-Requested-With, Content-Type, Accept, " + "X-CSRF-TOKEN");
-
+        //todo sprawdzi czy authorization z malej jest potrzebne
+        res.setHeader(HEADERS_ALLOWED,
+                "Origin, authorization, Authorization, X-Requested-By, X-Requested-With, Content-Type, Accept, " + "X-CSRF-TOKEN");
         res.setHeader("Access-Control-Expose-Headers", HEADER_STRING);
         res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
     }
